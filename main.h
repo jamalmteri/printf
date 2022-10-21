d61dde27e24999d5b448d6677f72758c8d727df3
gonjetso <gonjetsoc@gmail.com>
2022-10-21 06:53:01 -0700

    Update main

--- main.h
+++ main.h
@@ -24,16 +24,16 @@ int _printf(const char *format, ...);
 int _write_char(char);
 int print_char(va_list);
 int print_string(va_list);
-int print_percent(va_list);
 int print_integer(va_list);
 int print_number(va_list);
 int print_binary(va_list);
 int print_reversed(va_list arg);
-int rot13(va_list);
 int unsigned_integer(va_list);
 int print_octal(va_list list);
 int print_hex(va_list list);
 int print_heX(va_list list);
+int rot13(va_list);
+int print_percent(va_list);
 
 /*Helper functions*/
 unsigned int base_len(unsigned int, int);
